5dee2d2c5669d17d740f605e97ab96351ae1e46e
uram931 <59157588+uram931@users.noreply.github.com>
2020-01-05 21:39:28 +0900

    vanila-js__theory
    (variables)keep doing~~~

---
+++ index.js
@@ -0,0 +1,12 @@
+// 자바스크립트는 좋지 않은 문법을 사용해도 그에 대해 잘 알려주지 않는다
+// 각각의 라인(문장)들을 Expressions라고 칭한다
+// 모든 expression은 한줄에 있어야 하며,모든 instruction 또한 한줄에 있어야 한다
+// 변수 >>>생성,초기화,사용
+// let은 수학적 상태를 의미한다, 변수를 초기화하거나 생성할때에는 let을 써야 한다
+// alert("Im Working");
+// const=constant
+console.log("Im working");
+let a = 10;
+let b = a - 5;
+a = 4;
+console.log(b, a);
